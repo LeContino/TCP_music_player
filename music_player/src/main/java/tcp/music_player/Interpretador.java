443c5f2832d799afa70d4ea51ccdf6baf720a24e
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-09-27 22:43:18 -0300

    Verificação de comando inválidos

--- music_player/src/main/java/tcp/music_player/Interpretador.java
+++ music_player/src/main/java/tcp/music_player/Interpretador.java
@@ -9,7 +9,7 @@ import java.lang.Math;
  */
 public class Interpretador {
     private String texto;
-    private ArrayList<String> textoConvertido = new ArrayList<String>();
+    public ArrayList<String> textoConvertido = new ArrayList<String>();
     private static final String[] comandosValidos = {
         // Notas musicais
         "a",
@@ -62,10 +62,20 @@ public class Interpretador {
         String comandoComposto = comando.substring(0,1);
         
         // Procurando por "btm+", "btm-" e "else"
-        if("b".equals(comando) || "e".equals(comando)){
+        if(
+            comando.length() >= 4 && 
+            ("bpm-".equals(comando.substring(0,4)) || 
+             "bpm+".equals(comando.substring(0,4)) || 
+             "else".equals(comando.substring(0,4))))
+        {
             comandoComposto = comando.substring(0,4);
         // Procurando por "t-", "t+" e "nl"
-        }else if("t".equals(comando) || "n".equals(comando)){
+        }else if(
+            comando.length() >= 2 &&
+            ("t+".equals(comando.substring(0,2)) || 
+             "t-".equals(comando.substring(0,2)) || 
+             "nl".equals(comando.substring(0,2))))
+        {
             comandoComposto = comando.substring(0,2);
         }
         return comandoComposto;
@@ -80,12 +90,20 @@ public class Interpretador {
         for(int i = 0; i < this.texto.length(); i++){
             // Temos que verificar qual é o tamanho do comando mais longo para não acessar um índice inválido
             if(comprimentoTexto-i > comandoMaisLongo){
+                System.out.println(this.texto.substring(i,i+comandoMaisLongo));
                 comandoComposto = this.verificarComandoComposto(this.texto.substring(i,i+comandoMaisLongo));
             }else{
+                System.out.println(this.texto.substring(i));
                 comandoComposto = this.verificarComandoComposto(this.texto.substring(i));
             }
             
+            // Verificando se o comando encontrado é um comando válido
+            boolean comandoValido = Arrays.stream(comandosValidos).anyMatch(comandoComposto::equals);
+            if(!comandoValido) return false;
+            
             this.textoConvertido.add(comandoComposto);
+            
+            if(comandoComposto.length() > 1) i += comandoComposto.length()-1;
         }
         
         return true;
